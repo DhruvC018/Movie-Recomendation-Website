4e04a20114eb6088f98b80fe3884bb24319f82c5
DhruvC018 <59206148+DhruvC018@users.noreply.github.com>
2021-08-17 23:20:53 +0530

    Used concepts like middleware and currying

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 // import { render } from '@testing-library/react';
 import React from 'react';
+import { data } from '../data';
+// import {add}
 
 class Navbar extends React.Component{
     render() {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './reducers';
 
+//We can have multiple middlewares
+
+//here we see a curried fucntion. 
+//can also be expressed as function logger(obj,next,action) //here the obj is {dispatch,getState}.
+//using currying Expressd aslogger(obj)(next)(action)
+
+// const logger = function ({dispatch, getState}) { //these are the simple parameters containing the dispatch and getState.
+//   return function(next){ //currying used here.
+//     return function (action){
+//       //middleware code
+//       console.log("ACTION_TYPE = ", action.type);
+//       next(action); //we don't want out app to be stuck and we want it to continue to move on. Hence we do the next like we do a next in a linkedlist.
+//     }
+//   }
+// }
+
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  console.log("ACTION_TYPE = ", action.type);
+  next(action);
+}
 
 //movies is the reducer name.
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store',store);
 // console.log('Before sending the action state',store.getState());
 
